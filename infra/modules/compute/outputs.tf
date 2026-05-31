0485401e9a776a5c9f81261bdcec3cb33199da49
Keval Kapdee <keval@kapdee.uk>
2026-05-31 19:58:29 +0100

    Strip trailing slash from Lambda URL host for CloudFront origin

--- infra/modules/compute/outputs.tf
+++ infra/modules/compute/outputs.tf
@@ -1,2 +1,2 @@
 output "api_lambda_url"      { value = aws_lambda_function_url.api.function_url }
-output "api_lambda_url_host" { value = replace(aws_lambda_function_url.api.function_url, "https://", "") }
+output "api_lambda_url_host" { value = trimsuffix(trimprefix(aws_lambda_function_url.api.function_url, "https://"), "/") }
